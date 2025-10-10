00c89c0e7b83bbd4d6c34c72f485cdb8b54deaca
João <joao.rvc.pinho@gmail.com>
2025-10-10 13:11:18 +0100

    add initial sql code for task2

---
+++ task2/populate.sql
@@ -0,0 +1,35 @@
+SET NAMES utf8;
+
+DROP DATABASE IF EXISTS project;
+
+CREATE DATABASE IF NOT EXISTS project DEFAULT CHARACTER SET utf8;
+
+USE project;
+
+DROP TABLE IF EXISTS random_data;
+
+CREATE TABLE random_data (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_name VARCHAR(100),
+    price INT,
+    signup_date DATE,
+    postcode VARCHAR(10),
+    unknown1 VARCHAR(100),
+    unknown2 VARCHAR(100),
+    unknown3 VARCHAR(100),
+    paon VARCHAR(100),
+    saon VARCHAR(100),
+    street VARCHAR(100),
+    locality VARCHAR(100),
+    town VARCHAR(100),
+    district VARCHAR(100),
+    county VARCHAR(100),
+    unknown4 VARCHAR(100),
+    unknown5 VARCHAR(100)
+);
+
+
+load data local infile '../pp-2025.csv'
+into table random_data
+fields terminated by ','
+lines terminated by '\n';
