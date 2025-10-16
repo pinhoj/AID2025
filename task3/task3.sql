bf0de473ae1f49625a701f77ae7cf9d481837794
AfonsoMateus12 <afonso.mateus@tecnico.ulisboa.pt>
2025-10-16 10:40:18 +0100

    task 3 query now by date too

--- task3/task3.sql
+++ task3/task3.sql
@@ -1,5 +1,5 @@
-SELECT county, district, town, COUNT(1) AS sales_number
+SELECT county, district, town, signup_date, COUNT(1) AS sales_number
 FROM project_data
-GROUP BY county, district, town
+GROUP BY county, district, town, signup_date
 ORDER BY sales_number DESC
 LIMIT 10;
